d5c879b7aeb5ae8eeb979b4416d513bb24e0287c
jirvingphd <james.irving.phd@gmail.com>
2019-10-22 00:50:59 -0400

    Bump version: 0.2.3 → 0.3.0

--- fsds_100719/__init__.py
+++ fsds_100719/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '0.2.3'
+__version__ = '0.3.0'
 from .imports import *
 
 print(f"fsds_1007219  v{__version__} loaded.  Read the docs: https://fsds.readthedocs.io/en/latest/ ")
--- setup.py
+++ setup.py
@@ -47,6 +47,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/jirvingphd/fsds_100719',
-    version='0.2.3',
+    version='0.3.0',
     zip_safe=False,
 )
