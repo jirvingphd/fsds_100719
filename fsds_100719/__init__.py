476a8fdc660748c1391b6658117af423c7787275
jirvingphd <james.irving.phd@gmail.com>
2019-10-23 02:14:44 -0400

    updated init

--- fsds_100719/__init__.py
+++ fsds_100719/__init__.py
@@ -5,4 +5,4 @@ __version__ = '0.3.7'
 print(f"fsds_1007219  v{__version__} loaded.  Read the docs: https://fsds.readthedocs.io/en/latest/ ")
 print(f"> For convenient loading of standard modules use: `>> from fsds_100719.imports import *`\n")
 
-from .ds import ihelp
+from .ds import ihelp, ihelp_menu,quick_ref
