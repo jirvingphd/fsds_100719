5456cd9d5dc42d299fb9f8f324f242623f86a8b1
jirvingphd <james.irving.phd@gmail.com>
2019-10-22 20:24:38 -0400

    fixing runapidoc params

--- docs/conf.py
+++ docs/conf.py
@@ -183,7 +183,7 @@ def run_apidoc(_):
 	sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 	cur_dir = os.path.abspath(os.path.dirname(__file__))
 	module = os.path.join(cur_dir,"..","fsds_100719")
-	main(['-o', cur_dir, module,'-M -E','--force'])
+	main(['-o', cur_dir, module,'-E ','-M','--force'])
 
 def setup(app):
 	app.connect('builder-inited', run_apidoc)
