5d57fe83bb91acddb8b1cb072329b2fa3439af23
jirvingphd <james.irving.phd@gmail.com>
2019-10-23 02:13:15 -0400

    removed imports in base __init__

--- fsds_100719/__init__.py
+++ fsds_100719/__init__.py
@@ -1,7 +1,7 @@
 __version__ = '0.3.6'
-from .imports import *
-from .ft import *
-from .pt import * 
+# from .imports import *
+# from .ft import *
+# from .pt import * 
 print(f"fsds_1007219  v{__version__} loaded.  Read the docs: https://fsds.readthedocs.io/en/latest/ ")
 print(f"> For convenient loading of standard modules use: `>> from fsds_100719.imports import *`\n")
 
