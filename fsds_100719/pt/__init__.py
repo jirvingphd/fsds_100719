5bb43ab5a8546eecf46877d50ad543321084ac88
jirvingphd <james.irving.phd@gmail.com>
2019-10-23 02:02:28 -0400

    moved shared code to ds submodule

--- fsds_100719/pt/__init__.py
+++ fsds_100719/pt/__init__.py
@@ -2,21 +2,22 @@
 Maintained by James Irving (GitHub: jirvingphd)
 james.irving@flatironschool.com
 """
+# from .ds import 
 # from ..imports import *
-def quick_ref(topic='student_resource_folder'):
-    """Displays quick reference url links and info.
-    Args:
-        topic (str): selects which reference info to show.
-            - `student_resource_folder` : data science gdrive url
-            - `fsds` :documentaion url"""
+# def quick_ref(topic='student_resource_folder'):
+#     """Displays quick reference url links and info.
+#     Args:
+#         topic (str): selects which reference info to show.
+#             - `student_resource_folder` : data science gdrive url
+#             - `fsds` :documentaion url"""
             
-    if 'student_resource_folder' in topic:
-        print('Data Science Student Resources:')
-        print('https://flatiron.online/StudentResourcesGdrive')
+#     if 'student_resource_folder' in topic:
+#         print('Data Science Student Resources:')
+#         print('https://flatiron.online/StudentResourcesGdrive')
         
-    if 'fsds' in topic:
-        print('fsds_100719 Package Documentation:')
+#     if 'fsds' in topic:
+#         print('fsds_100719 Package Documentation:')
         
         
-def placeholder():
+def placeholder2():
     pass
