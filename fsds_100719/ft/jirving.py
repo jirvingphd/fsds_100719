41ba38bb3a1bdcc0f422694e038c8d6b6f8698ba
jirvingphd <james.irving.phd@gmail.com>
2019-11-07 12:53:54 -0500

    example upload

--- fsds_100719/ft/jirving.py
+++ fsds_100719/ft/jirving.py
@@ -10,3 +10,34 @@ from fsds_100719.jmi import reload, print_docstring_template
 
 def testing():
     print('Huzzah!')
+    
+def check_column(df, col_name, n_unique=10):
+    """Displays info on null values, datatype, unqiue values
+    and displays .describe()
+    
+    Args:
+        df (df): contains the columns
+        col_name (str): name of the df column to show
+        n_unique (int): Number of unique values top show.
+    
+    Return:
+        fig, ax (Matplotlib Figure and Axes)
+    """
+    import matplotlib.pyplot as plt
+    from IPython.display import display
+    print('DataType:')
+    print('\t',df[col_name].dtypes)
+    
+    num_nulls = df[col_name].isna().sum()
+    print(f'Null Values Present = {num_nulls}')
+    
+    display(df[col_name].describe().round(3))
+    
+    print('\nValue Counts:')
+    display(df[col_name].value_counts(n_unique))
+    
+    ## Add some EDA figures
+    fig, ax = plt.subplots(ncols=2)
+    
+    return fig,ax
+    
