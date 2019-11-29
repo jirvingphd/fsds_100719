7213402575c2bfeb2d2a8e1e429b44235eb928f9
Hakkeray <hakkeray@gmail.com>
2019-11-29 12:41:58 -0500

    added hakkeray package

--- fsds_100719/ft/jirving.py
+++ fsds_100719/ft/jirving.py
@@ -4,8 +4,8 @@ Email: james.irving.phd@gmail.com
 GitHub Profile: https://github.com/jirvingphd
 """
 # import fsds_100719 as fs 
-from fsds_100719.ds import ihelp,ihelp_menu
-from fsds_100719.jmi import reload, print_docstring_template
+from fsds_100719.ds import ihelp,ihelp_menu, reload
+from fsds_100719.jmi import print_docstring_template
 # print(f"[i] You're using V {fs.__version__} of fsds.")
 
 def testing():
