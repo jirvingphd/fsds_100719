e8f4028305f53394a12726376d1d74f58c922e10
jirvingphd <james.irving.phd@gmail.com>
2019-11-29 16:23:02 -0500

    Bump version: 0.4.42 → 0.4.43

--- fsds_100719/__init__.py
+++ fsds_100719/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '0.4.42'
+__version__ = '0.4.43'
 # from .imports import *
 # from .ft import *
 # from .pt import * 
--- setup.py
+++ setup.py
@@ -48,6 +48,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/jirvingphd/fsds_100719',
-    version='0.4.42',
+    version='0.4.43',
     zip_safe=False,
 )
